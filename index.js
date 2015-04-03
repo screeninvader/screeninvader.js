bccc73cdd5a67280f9ed56b11a101e1108118812
phaer <phaer@phaer.org>
2015-04-03 04:06:17 +0200

    breaking changes: incremental state updates.

--- index.js
+++ index.js
@@ -1,31 +1,62 @@
+var Player = function(api) {
+    this.api = api;
+    //['Pause', 'Stop', 'Next', 'Previous', 'Forward', 'Rewind', 'Jump']
+};
+
+Player.prototype = {
+    pause: function() { this.api.command('playerPause'); },
+    stop: function() { this.api.command('playerStop'); },
+    next: function() { this.api.command('playerNext'); },
+    previous: function() { this.api.command('playerPrevious'); },
+    forward: function() { this.api.command('playerForward'); },
+    rewind: function() { this.api.command('playerRewind'); },
+    jump: function(id) {
+        this.api.command('playerJump', parseInt(id) + 1);
+    },
+};
+
 var API = function(uri) {
-    this.callbacks = [];
     this.socket = new WebSocket(uri);
     this.socket.onmessage = this.onMessage.bind(this);
     this.socket.onerror = this.onError.bind(this);
     this.socket.onopen = this.onOpen.bind(this);
-
-    this.player = this.addCommandSet(
-        'player',
-        ['Pause', 'Stop', 'Next', 'Previous', 'Forward', 'Rewind', 'ForwardMore',
-         'RewindMore', 'Jump']);
-    this.browser = this.addCommandSet(
-        'browser',
-        ['PageUp', 'PageDown', 'ScrollUp', 'ScrollDown', 'ZoomIn', 'ZoomOut']);
-
+    this.player = new Player(this);
+    //browser = ['PageUp', 'PageDown', 'ScrollUp', 'ScrollDown', 'ZoomIn', 'ZoomOut']);
 };
 
 API.prototype = {
-    onReceive: function(callback) {
-        this.callbacks.push(callback);
+    onReady: function(fn) {
+        this.callback = fn;
     },
     onMessage: function(ev) {
-        var self = this;
-        self.state = JSON.parse(ev.data);
-        console.debug('received state update', self.state);
-        this.callbacks.forEach(function(callback) {
-            callback.call(self, self.state);
-        });
+        var update = JSON.parse(ev.data);
+        if (!Array.isArray(update)) {
+            // initial full sync.
+            this.state = update;
+            this.callback(this.state);
+            return;
+        }
+        // update has the format key, operation, value here.
+        var path = update[0].split('/');
+        path.shift();
+        console.debug('changing ' + update[0] + ' from ' +
+                      this.getByPath(this.state, path.slice(0)) +
+                      ' to ' + update[2]);
+        this.setByPath(this.state, path.slice(0), update[2]);
+    },
+    setByPath: function (obj, path, value) {
+        if (path.length > 1) {
+            return this.setByPath(obj[path.shift()], path, value);
+        } else {
+            obj[path.shift()] = value;
+        }
+    },
+    getByPath: function(obj, path) {
+        if (path.length > 0) {
+            return this.getByPath(obj[path.shift()], path);
+        } else {
+            return obj;
+        }
     },
     onError: function(ev) {
         console.error(ev);
@@ -33,7 +64,6 @@ API.prototype = {
     onOpen: function(ev) {
         this.socket.send('setup');
     },
-
     send: function(command, key, value) {
         this.socket.send(
             JSON.stringify(
@@ -51,15 +81,6 @@ API.prototype = {
     },
     showUrl: function(url) { this.command("showUrl", url); },
     setVolume: function(volume) { this.set('/sound/volume', volume); },
-
-    addCommandSet: function(name, methods) {
-        var set = {}, self = this;
-        methods.forEach(function(method, index) {
-            set[method.toLowerCase()] = function(param) {
-                self.command(name + method, param); };
-        });
-        return set;
-    }
 };
 
 module.exports = API;
