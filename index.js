c4f28f699f8ad861fc2985c046f985ecdb823c60
Jascha Ehrenreich <jascha@jaeh.at>
2015-04-04 03:28:28 +0200

    changed indentation

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 var Player = function(api) {
     this.api = api;
-};
+}
 
 Player.prototype = {
-    pause: function() { this.api.command('playerPause'); },
+    pause:    function() { this.api.command('playerPause'); },
     // pause a second time un-pauses/plays.
-    play: function() { this.api.command('playerPause'); },
-    stop: function() { this.api.command('playerStop'); },
-    next: function() { this.api.command('playerNext'); },
+    play:     function() { this.api.command('playerPause'); },
+    stop:     function() { this.api.command('playerStop'); },
+    next:     function() { this.api.command('playerNext'); },
     previous: function() { this.api.command('playerPrevious'); },
-    forward: function() { this.api.command('playerForward'); },
-    rewind: function() { this.api.command('playerRewind'); },
-    jump: function(id) {
+    forward:  function() { this.api.command('playerForward'); },
+    rewind:   function() { this.api.command('playerRewind'); },
+    jump:     function(id) {
         this.api.command('playerJump', parseInt(id) + 1);
     },
 };
@@ -22,13 +22,15 @@ var API = function(uri) {
     this.socket.onerror = this.onError.bind(this);
     this.socket.onopen = this.onOpen.bind(this);
     this.player = new Player(this);
-    //browser = ['PageUp', 'PageDown', 'ScrollUp', 'ScrollDown', 'ZoomIn', 'ZoomOut']);
-};
+}
 
 API.prototype = {
+
+    //fn is a custom callback and gets executed in onMessage below
     onReceive: function(fn) {
         this.onReceiveCallback = fn;
     },
+    //message from the server received
     onMessage: function(ev) {
         var update = JSON.parse(ev.data);
         if (!Array.isArray(update)) {
@@ -59,29 +61,38 @@ API.prototype = {
             return obj;
         }
     },
+    //socket connection errored
     onError: function(ev) {
         console.error(ev);
     },
+    //first response
     onOpen: function(ev) {
         this.socket.send('setup');
     },
+    //send a message to the server
     send: function(command, key, value) {
         this.socket.send(
             JSON.stringify(
                 Array.prototype.slice.call(
                     arguments)));
     },
+    //set a serverside janosh key to a value
     set: function (key, value) {
         console.debug('setting', key, 'to', value);
         this.send('trigger', key, value);
     },
+    //classes use this to execute commands
     command: function(command, param) {
         param = typeof(param) === 'undefined' ? '' : param.toString();
         console.debug('executing '+command+'('+param+')');
         this.send('publish', command, 'W', param);
     },
+    //show a url in the browser, pdfviewer or videoplayer
     showUrl: function(url) { this.command("showUrl", url); },
+    //set the volume to value
     setVolume: function(volume) { this.set('/sound/volume', volume); },
+    //mute and unmute are the same function
+    mute: function(volume) { this.set('/sound/mute', volume); },
 };
 
 module.exports = API;
